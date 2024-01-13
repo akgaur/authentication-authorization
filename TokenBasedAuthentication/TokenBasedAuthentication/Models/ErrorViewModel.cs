970110afbb84d7abfdda32d36b64bb0cfe0240cf
Akash kumar gaur <gaurakash41@gmail.com>
2024-01-13 17:55:59 +0530

    Delete TokenBasedAuthentication directory
    Delete to remove garbage

--- TokenBasedAuthentication/TokenBasedAuthentication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TokenBasedAuthentication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
